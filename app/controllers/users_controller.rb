a8d2875fdf4f89e43b775aafaa34820235676c18
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-15 22:48:22 +0300

    update users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    user_params = params.require(:user).permit(:username)
   end
+
 end
