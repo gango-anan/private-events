5b17312222f4ed20105577e062397fc104989249
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-15 18:24:22 +0300

    add Attendance model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_142701) do
+ActiveRecord::Schema.define(version: 2021_02_15_151302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_attendances_on_event_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "event_name", null: false
@@ -33,5 +42,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_142701) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "attendances", "events"
+  add_foreign_key "attendances", "users"
   add_foreign_key "events", "users", column: "creator_id"
 end
---
+++ spec/models/attendance_spec.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  event_id   :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_attendances_on_event_id  (event_id)
+#  index_attendances_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_id => events.id)
+#  fk_rails_...  (user_id => users.id)
+#
+require 'rails_helper'
+
+RSpec.describe Attendance, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
