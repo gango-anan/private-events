8817bf3e65fb1074581f3adf636a1045a933fcc1
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-16 20:50:39 +0300

    generate events controller and views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,16 @@
+class EventsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/create'
+  get 'events/update'
+  get 'events/destroy'
   root 'users#index'
   resources :users, only: [:new, :show, :create]
   get 'signout' => 'logins#destroy', as: 'signout'
