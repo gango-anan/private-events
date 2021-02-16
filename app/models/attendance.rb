93e42eab47539c6c661b488b6d848008154497a0
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-16 19:06:40 +0300

    run migration to rename attendee_id back to user_id to fix the error where an attendee couldnt see the events he/she is invited to

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,23 +2,23 @@
 #
 # Table name: attendances
 #
-#  id          :bigint           not null, primary key
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  attendee_id :bigint           not null
-#  event_id    :bigint           not null
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  event_id   :bigint           not null
+#  user_id    :bigint           not null
 #
 # Indexes
 #
-#  index_attendances_on_attendee_id  (attendee_id)
-#  index_attendances_on_event_id     (event_id)
+#  index_attendances_on_event_id  (event_id)
+#  index_attendances_on_user_id   (user_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (attendee_id => users.id)
 #  fk_rails_...  (event_id => events.id)
+#  fk_rails_...  (user_id => users.id)
 #
 class Attendance < ApplicationRecord
-  belongs_to :user, foreign_key: "attendee_id", class_name: "User"
+  belongs_to :user
   belongs_to :event
 end
--- spec/factories/attendances.rb
+++ spec/factories/attendances.rb
@@ -2,21 +2,21 @@
 #
 # Table name: attendances
 #
-#  id          :bigint           not null, primary key
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  attendee_id :bigint           not null
-#  event_id    :bigint           not null
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  event_id   :bigint           not null
+#  user_id    :bigint           not null
 #
 # Indexes
 #
-#  index_attendances_on_attendee_id  (attendee_id)
-#  index_attendances_on_event_id     (event_id)
+#  index_attendances_on_event_id  (event_id)
+#  index_attendances_on_user_id   (user_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (attendee_id => users.id)
 #  fk_rails_...  (event_id => events.id)
+#  fk_rails_...  (user_id => users.id)
 #
 FactoryBot.define do
   factory :attendance do
