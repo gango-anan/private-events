b15d9e7e8d5191478b19c593ffe6c2168a49226d
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-20 13:59:48 +0300

    update routes to only allow the required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'events#index'
   resources :attendances, only: [:new, :create]
-  resources :events
+  resources :events, only: [:index, :new, :show, :create]
   resources :users, only: [:index, :new, :show, :create]
   get 'signout' => 'logins#destroy', as: 'signout'
   resources :logins, only: [:new, :create]
