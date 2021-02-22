cafb592517ffcc2a27c6064e675a25391dbc2b63
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-22 21:04:59 +0300

    add events integration tests

--- spec/requests/events_request_spec.rb
+++ spec/requests/events_request_spec.rb
@@ -1,38 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Events', type: :request do
-  # describe 'GET /index' do
-  #   it 'returns http success' do
-  #     get '/events/index'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/events/index'
+      expect(response).to have_http_status(200)
+    end
+  end
 
-  # describe 'GET /new' do
-  #   it 'returns http success' do
-  #     get '/events/new'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/events/new'
+      expect(response).to have_http_status(200)
+    end
+  end
 
-  # describe 'GET /create' do
-  #   it 'returns http success' do
-  #     get '/events/create'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/events/create'
+      expect(response).to have_http_status(200)
+    end
+  end
 
-  # describe 'GET /update' do
-  #   it 'returns http success' do
-  #     get '/events/update'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe 'GET /destroy' do
-  #   it 'returns http success' do
-  #     get '/events/destroy'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe 'POST /create' do
+    it 'returns http success' do
+      event = { description: 'Hello', event_date: Time.now, event_name: 'G Party', event_venue: 'Cafe Ark', creator_id: 1 }
+      post events_path, params: event
+      expect(response).to have_http_status(200)
+    end
+  end
 end
