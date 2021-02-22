e4a1c12bb3c1f2aa2446d5c4405616ded25fc2d5
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-22 19:41:28 +0300

    remove unused code and add integration tests for users

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
       get '/users/index'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /new' do
     it 'returns http success' do
       get '/users/new'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
       get '/users/show'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /create' do
     it 'returns http success' do
       get '/users/create'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
     end
   end
 end
