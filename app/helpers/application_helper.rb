b3f11535807f9412a3ec1c533b4481efc04e1073
Galiwango Ananiya <galiwango101@gmail.com>
2021-02-22 22:07:31 +0300

    remove logic from the header partial view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def current_user
     User.where(id: session[:user_id]).first
   end
+
+  def logged_in?
+    return render 'layouts/unsigned_in_user' unless current_user
+    render 'layouts/signed_in_user'
+  end
 end
